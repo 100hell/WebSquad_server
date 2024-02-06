4d4c3254b80c1dab94316b57fb9c82fec96f0de2
100hell <99403164+100hell@users.noreply.github.com>
2024-02-06 17:28:10 +0530

    Update connectDB.js

--- db/connectDB.js
+++ db/connectDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect("mongodb+srv://sohelkhan:txpLyATKJfR30PKg@cluster0.rrqrvxk.mongodb.net/web-squad?retryWrites=true&w=majority", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
